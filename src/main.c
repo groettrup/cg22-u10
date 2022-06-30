8d4a20a763347499edc66a96452b87a0bc7a740b
Alexander Gröttrup <groettrup@fu-berlin.de>
2022-06-30 14:53:21 +0200

    feat: basic GLFW error handling

--- src/main.c
+++ src/main.c
@@ -3,9 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void error_callback(int error, const char* description)
+{
+  fprintf(stderr, "Error: %s\n", description);
+}
+
 int main(void)
 {
   if (!glfwInit())
     exit(EXIT_FAILURE);
-  return 0;
+
+  // Handle Errors
+  glfwSetErrorCallback(error_callback);
+
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
