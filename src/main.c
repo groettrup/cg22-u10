cdf27c76f7df17b66f13513406448c9b954d3e0d
Alexander Gröttrup <groettrup@fu-berlin.de>
2022-06-30 15:05:37 +0200

    feat: create basic window

--- src/main.c
+++ src/main.c
@@ -10,12 +10,36 @@ static void error_callback(int error, const char* description)
 
 int main(void)
 {
+  // Handler for the window object
+  GLFWwindow* window;
+
+  // Try to initialize GLFW
   if (!glfwInit())
     exit(EXIT_FAILURE);
 
   // Handle Errors
   glfwSetErrorCallback(error_callback);
 
+  // Try to create Window
+  window = glfwCreateWindow(200, 200, "homework", NULL, NULL);
+  if(!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  // Make context current on main thread
+  glfwMakeContextCurrent(window);
+
+  // Main loop
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
